b3ee2f7391233cee18e8a35456cae9daa712b698
rajika111 <poower@gmail.com>
2019-10-09 00:57:42 +0300

    raji

---
+++ aboutMe/apps.js
@@ -0,0 +1,55 @@
+//Q1
+const page=document.querySelector("page");
+style.fontfamily="Arial, sans-serif"
+//Q2
+const nickname = document.querySelector('#nickname');  
+const favorite = document.querySelector('#favorite'); 
+const hometown = document.querySelector('#hometown'); 
+
+nickname.innerHTML = "rayG";
+favorite.innerHTML = "playing video games"; 
+hometown.innerHTML = "Riyadh";
+
+const li=document.querySelector('li');
+for(let listitem= 
+  listitem.class.add('listitem');
+   listitemstyle.color="rebeccapurple";
+}
+
+//q3,4
+const pic=document.createElement('img'); ('src', 'https://picsum.photos/id/214/200/300')
+const profilepicture=document.querySelector('profilepicture');
+//list of books
+var books = [
+    {
+      title: "The Design of Everyday Things",
+      author: "Don Norman",
+      alreadyRead: false
+    },
+    {
+      title: "The Most Human Human",
+      author: "Brian Christian",
+      alreadyRead: true
+    },
+    {
+      title: "In Search of Lost Time",
+      author: "Marcel Proust",
+      alreadyRead: true
+    },
+    {
+      title: "Ulysses",
+      author: "James Joyce",
+      alreadyRead: true
+    },
+    {
+      title: "The Great Gatsby",
+      author: "F. Scott Fitzgerald",
+      alreadyRead: true
+    }
+  ];
+
+  //booklist
+const mybooks=document.createElement('h1')
+mybooks.innerText="My Book List"
+
+
