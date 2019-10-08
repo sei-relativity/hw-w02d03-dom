6be8184de73981a7a1daabd656b953dc6491a123
RayG <54848869+rajika111@users.noreply.github.com>
2019-10-09 01:14:16 +0300

    random pic

---
+++ apps.js
@@ -0,0 +1,11 @@
+
+const newPic = document.createElement ('img') ///create new elm newPic 
+newPic.setAttribute('src,https://picsum.photos/id/1003/1181/1772');
+newPic.setAttribute('src,https://picsum.photos/id/1014/6016/4000');
+
+const width=document.querySelector('.width');
+const height=document.querySelector('.height');
+
+ randomize.addEventListener("click", randomimg);
+function randomImag(){
+const newPic = document.querySelector('.image'); newPic.appendChild(newPic);
