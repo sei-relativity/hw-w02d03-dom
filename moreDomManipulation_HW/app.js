be13fe98eac3812c8db27e230c3fe9a5778b114e
almarrim <mosalmarri@gmail.com>
2019-10-08 22:38:34 +0300

    hw w2d3 moredom theBookList part+ moved homework files into their respective folders

--- moreDomManipulation_HW/app.js
+++ moreDomManipulation_HW/app.js
@@ -17,3 +17,41 @@ for (i of listitem) {
 const newImg = document.createElement('img');
 newImg.setAttribute('src', 'timon.png');
 document.querySelector('div.profilePicture').appendChild(newImg);
+
+////The Book List
+var books = [
+    {
+        title: "The Design of Everyday Things",
+        author: "Don Norman",
+        alreadyRead: false
+    },
+    {
+        title: "The Most Human Human",
+        author: "Brian Christian",
+        alreadyRead: true
+    },
+    {
+        title: "In Search of Lost Time",
+        author: "Marcel Proust",
+        alreadyRead: true
+    },
+    {
+        title: "Ulysses",
+        author: "James Joyce",
+        alreadyRead: true
+    },
+    {
+        title: "The Great Gatsby",
+        author: "F. Scott Fitzgerald",
+        alreadyRead: true
+    }
+];
+const newh1 = document.createElement('h1');
+newh1.innerText = 'My Book List';
+document.querySelector('div.favoriteBooks').appendChild(newh1);
+
+for (i of books) {
+    const newbook = document.createElement('p');
+    newbook.innerText = i.title + ', ' + i.author;
+    document.body.appendChild(newbook)
+}
