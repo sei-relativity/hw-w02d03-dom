4a12c58bacfa2bc7d3d0e3806d8416f910da1cff
Sager Alarifi <sagr.1993@gmail.com>
2019-10-09 07:28:36 +0300

    add images and link JS

---
+++ moreDomManipulation/app.js
@@ -0,0 +1,28 @@
+const books = [
+    {
+        title: "The Design of Everyday Things",
+        author: "Don Norman",
+        alreadyRead: false
+    },
+    {
+        title: "The Most Human Human",
+        author: "Brian Christian",
+        alreadyRead: true
+    },
+    {
+        title: "In Search of Lost Time",
+        author: "Marcel Proust",
+        alreadyRead: true
+    },
+    {
+        title: "Ulysses",
+        author: "James Joyce",
+        alreadyRead: true
+    },
+    {
+        title: "The Great Gatsby",
+        author: "F. Scott Fitzgerald",
+        alreadyRead: true
+    }
+];
+
---
+++ pictureGenerator/app.js
@@ -0,0 +1,20 @@
+const display_random_image = function(){
+    const images = [{
+        src: "http://i.imgur.com/z6yfE.jpg"
+    },{
+
+    }, {
+        src: "http://i.imgur.com/Q2Q1f.jpg"
+    }, {
+        src: "http://i.imgur.com/IiqX7.jpg"
+    },{
+
+    }, {
+        src: "http://i.imgur.com/EZUuy.jpg"
+    }, {
+        src: "http://i.imgur.com/IDFxr.jpg"
+    }
+    ]
+    let randomPic = images[Math.floor(Math.random()*myArray.length)];
+    
+}
