8097f50896f994d710802c43bb518839329e6bb3
نوف الوزير <noufalmatroudi@nwfs-MacBook-Air.local>
2019-10-09 09:02:43 +0300

    done

---
+++ main.js/home.js
@@ -0,0 +1,38 @@
+
+const style = document.querySelectorAll('body');
+style.style..style.fontFamily="Arial, sans-serif";
+
+const listItem = document.querySelectorAll('li');
+listItem.setAttribute('listitem');
+
+const textColor = document.querySelectorAll('li');
+textColor.style.color = "rebeccapurple";
+
+//
+var books = [
+    {
+      title: "The Design of Everyday Things",
+      author: "Don Norman",
+      alreadyRead: false
+    },
+    {
+      title: "The Most Human Human",
+      author: "Brian Christian",
+      alreadyRead: true
+    },
+    {
+      title: "In Search of Lost Time",
+      author: "Marcel Proust",
+      alreadyRead: true
+  },
+  {
+    title: "Ulysses",
+    author: "James Joyce",
+    alreadyRead: true
+  },
+  {
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    alreadyRead: true;
+  }
+];
