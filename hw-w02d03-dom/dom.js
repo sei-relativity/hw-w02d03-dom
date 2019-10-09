33c3ab7f3804714b95c1cd74174e6a6f732b0695
Abdullahjr <55585730+Abdullahjr@users.noreply.github.com>
2019-10-09 08:17:14 +0300

    Add files via upload

---
+++ hw-w02d03-dom/dom.js
@@ -0,0 +1,6 @@
+const font = document.getElementsByTagName('body');
+font .style.font-family = ('Arial, sans-serif');
+
+const name = document.getElementById('nickname');
+const fav =  document.getElementById('favorite');
+const home = document.getElementById('hometown');
