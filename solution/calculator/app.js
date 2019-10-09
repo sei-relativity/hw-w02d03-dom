24746d3a3ac0855a1d8d545ed0371758697b3149
AlShabibi <shabams@gmail.com>
2019-10-09 09:45:16 +0300

    finished the bonus 1

---
+++ solution/calculator/app.js
@@ -0,0 +1,29 @@
+const solution = document.createElement("h3");
+
+const halfIt = () => {
+  const input = document.querySelector("#half-input").value;
+  const result = input / 2;
+  console.log(result);
+  solution.innerText = `The result is ${result}`;
+  document.querySelector("#solution").appendChild(solution);
+};
+
+const fraction = () => {
+  const percentInput1 = document.querySelector("#percent1-input").value;
+  const percentInput2 = document.querySelector("#percent2-input").value;
+  const result = (percentInput1 / percentInput2) * 100;
+  solution.innerText = `The result is % ${result}`;
+  document.querySelector("#solution").appendChild(solution);
+};
+
+const area = () => {
+  const input = document.querySelector("#area-input").value;
+  const result = Math.PI * input * input;
+  console.log(result);
+  solution.innerText = `The result is ${result}`;
+  document.querySelector("#solution").appendChild(solution);
+};
+
+document.querySelector("#half-button").addEventListener("click", halfIt);
+document.querySelector("#percent-button").addEventListener("click", fraction);
+document.querySelector("#area-button").addEventListener("click", area);
