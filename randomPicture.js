fd20f6b11d66fb1ce5bc8ffa92b6305e7f0853c9
aba20 <askr1000@gmail.com>
2019-10-09 08:31:31 +0300

    Abdulrahman Alfouzan

---
+++ randomPicture.js
@@ -0,0 +1,8 @@
+// const newPic = document.getElementById('#randomize');
+// const botton = newPic.createElement('img');
+// botton.setAttribute('src', 'https://www.placecage.com/400/400');
+
+const getUrl = document.getElementById('#randomize')
+const url = getUrl.createElement('a');
+url.setAttribute('a', ' https://source.unsplash.com/1600x900')
+
