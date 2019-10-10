6c413f058fc25db31550787dfbb5abe6a2bf0d45
نوف الوزير <noufalmatroudi@nwfs-MacBook-Air.local>
2019-10-10 08:43:57 +0300

    done

--- main.js/home.js
+++ main.js/home.js
@@ -1,38 +1,19 @@
 
-const style = document.querySelectorAll('body');
-style.style..style.fontFamily="Arial, sans-serif";
 
-const listItem = document.querySelectorAll('li');
-listItem.setAttribute('listitem');
+const newImg = document.createElement('img');
 
-const textColor = document.querySelectorAll('li');
-textColor.style.color = "rebeccapurple";
+const img = document.querySelector('.image');
 
-//
-var books = [
-    {
-      title: "The Design of Everyday Things",
-      author: "Don Norman",
-      alreadyRead: false
-    },
-    {
-      title: "The Most Human Human",
-      author: "Brian Christian",
-      alreadyRead: true
-    },
-    {
-      title: "In Search of Lost Time",
-      author: "Marcel Proust",
-      alreadyRead: true
-  },
-  {
-    title: "Ulysses",
-    author: "James Joyce",
-    alreadyRead: true
-  },
-  {
-    title: "The Great Gatsby",
-    author: "F. Scott Fitzgerald",
-    alreadyRead: true;
-  }
-];
+
+img.appendChild(newImg)
+
+
+const src =function(){
+    
+    newImg.setAttribute('src','https://source.unsplash.com/random')
+};
+
+const button = document.querySelector('button#randomize');
+
+
+button.addEventListener('click',src)
